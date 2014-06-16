5ceaba15716079cdb8158957c1a7391708c4e1d8
Erin Staniland <erin@withassociates.com>
2014-06-16 08:47:58 +0100

    Move slices to dummy app

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ class PageConstraints
   end
 end
 
-Rails.application.routes.draw do
+Slices::Engine.routes.draw do
 
   devise_for :admin, :path => "admin", :controllers => {
     :passwords          => "admin/auth/passwords",
