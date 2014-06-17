864f8c6972c615a5c13cacd3f45cbf3d8a7f7c1c
Erin Staniland <erin@withassociates.com>
2014-06-17 10:33:42 +0100

    Remove ActiveRecord from dummy app

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,9 +1,11 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'sprockets/railtie'
 
 Bundler.require(*Rails.groups)
-require "slices"
+#require "slices"
 
 module Dummy
   class Application < Rails::Application
@@ -47,7 +49,7 @@ module Dummy
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,4 +1,2 @@
 Rails.application.routes.draw do
-
-  mount Slices::Engine => "/slices"
 end
