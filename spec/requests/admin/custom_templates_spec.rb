4c08c8c70e4f4808b46a76e84ebf97e9e5f8738c
Thomas Marshall <thomas@thomasmarshall.com>
2014-09-30 16:06:31 +0100

    Reset custom template config after tests

--- spec/requests/admin/custom_templates_spec.rb
+++ spec/requests/admin/custom_templates_spec.rb
@@ -3,9 +3,16 @@ require 'spec_helper'
 describe "Using custom template partials", type: :request do
   before do
     StandardTree.build_minimal
+    @page_actions_template = Slices::Config.page_actions_template
+    @page_fields_template = Slices::Config.page_fields_template
     Slices::Config.page_actions_template = 'custom/page_actions'
     Slices::Config.page_fields_template = 'custom/page_fields'
     sign_in_as_admin
+  end
+
+  after do
+    Slices::Config.page_actions_template = @page_actions_template
+    Slices::Config.page_fields_template = @page_fields_template
   end
 
   it "renders the configured page actions template" do
