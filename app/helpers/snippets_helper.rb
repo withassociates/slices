9bfdc18a8f28c97bd013479ffc57702f325caeee
Calum Gunn <calum@calumgunn.com>
2014-12-01 18:04:12 +0000

    #localized_snippet helper

--- app/helpers/snippets_helper.rb
+++ app/helpers/snippets_helper.rb
@@ -11,4 +11,16 @@ module SnippetsHelper
   def snippet(key = nil)
     Snippet.find_for_key(key)
   end
+
+  # Renders a localized snippet for the current locale
+  #
+  #   localized_snippet 'address'
+  #   # => "54B Downham Road"
+  #
+  # @param  [String]     key                         Snippet key to lookup
+  # @return [String]
+  #
+  def localized_snippet(key = nil)
+    snippet("#{I18n.locale}.#{key}")
+  end
 end
--- spec/helpers/snippets_helper_spec.rb
+++ spec/helpers/snippets_helper_spec.rb
@@ -2,29 +2,44 @@ require 'spec_helper'
 
 describe SnippetsHelper, type: :helper do
 
-  def stub_snippet(result)
+  def stub_snippet(key, result)
     expect(Snippet).to receive(:find_for_key).
-      with('address').
+      with(key).
       and_return(result)
   end
 
   context "#snippet" do
-    before do
-    end
-
     it "is the snippet" do
-      stub_snippet('54B Downham Road')
+      stub_snippet('address', '54B Downham Road')
       expect(snippet('address')).to eq '54B Downham Road'
     end
 
     it "allows HTML" do
-      stub_snippet('With Associates<br />54B Downham Road')
+      stub_snippet('address', 'With Associates<br />54B Downham Road')
       expect(snippet('address')).to eq 'With Associates<br />54B Downham Road'
     end
 
     it "handles missing keys" do
-      stub_snippet(nil)
       expect(snippet('address')).to be_nil
+    end
+  end
+
+  context "#localized_snippet" do
+    before { @original_locale = I18n.locale }
+    after { I18n.locale = @original_locale }
+
+    it "is the localized snippet" do
+      I18n.locale = :en
+      stub_snippet('en.address', '54B Downham Road')
+
+      expect(localized_snippet('address')).to eq '54B Downham Road'
+    end
+
+    it "is the localized snippet for a different locale" do
+      I18n.locale = :de
+      stub_snippet('de.address', '54B Untenschinken Strasse')
+
+      expect(localized_snippet('address')).to eq '54B Untenschinken Strasse'
     end
   end
 end
