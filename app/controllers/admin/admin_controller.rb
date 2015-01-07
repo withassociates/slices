57ab399a3c56fa97081a5f825bbd9b3b50a7885c
Graham Ashton <graham@effectif.com>
2015-01-07 10:07:42 +0000

    Set locale from query string, if available

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,5 +1,6 @@
 class Admin::AdminController < ActionController::Base
   before_filter :authenticate_admin!
+  around_filter :set_locale
 
   append_view_path(File.join(Rails.root, *%w[app slices]))
 
@@ -7,6 +8,18 @@ class Admin::AdminController < ActionController::Base
 
   def presenter_class(page_class)
     Object.const_get("#{page_class.name}Presenter")
+  end
+
+  private
+
+  def request_locale
+    params[:locale]
+  end
+
+  def set_locale
+    I18n.with_locale(request_locale || I18n.default_locale) do
+      yield
+    end
   end
 end
 
