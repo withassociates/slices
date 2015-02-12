f53fa8d8e222d7de0e3aa404c2db298be7d10d0c
Ben Symonds <ben@withassociates.com>
2015-02-12 12:51:28 +0000

    Fix rails deprecation warning (`enforce_available_locales`)

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -27,6 +27,8 @@ module Dummy
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    config.i18n.enforce_available_locales = true
+    config.i18n.available_locales = [:en, :de]
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
