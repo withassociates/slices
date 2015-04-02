268154bd6d2daed781e9363790d3552bf5ba97de
Erin Staniland <erin@withassociates.com>
2015-04-02 06:59:19 +0100

    Update dummy rails app configuration
    Using a combination of:
      rake rails:update
      rails new dummy -OT

--- spec/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '64b49ac7a4c38f7b19d47007bcc82b5da8765d071c1e94e24e8a015fe35a1404621164dd5f2da3bfa290b7b4604d25b840226771acb78366b70482c8c6f0884a'
