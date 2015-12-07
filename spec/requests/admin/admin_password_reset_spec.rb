bc9d1f01c485ea3389538cd028e07a09628d7a47
Erin Staniland <erin@withassociates.com>
2015-12-07 12:56:37 +0000

    tmp! Mark password reset as pending

--- spec/requests/admin/admin_password_reset_spec.rb
+++ spec/requests/admin/admin_password_reset_spec.rb
@@ -1,14 +1,15 @@
 require 'spec_helper'
 
 describe "Password reset for /admin", type: :request, js: true do
-  pending
 
   it "Reset admin users password" do
+    pending
+
     Devise::Mailer.default_url_options[:host] = "example.com"
     Admin.create!(email: 'hello@withassociates.com', password: '123456')
     StandardTree.build_minimal
 
-    visit '/admin/sign_in'
+    visit new_admin_session_path
     expect(page).to have_no_css('header li a', text: 'Log out')
 
     click_on 'Forgot your password?'
