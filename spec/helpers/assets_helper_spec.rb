76bf4657768070a6dd309845df1c675982c85f17
Erin Staniland <erin@withassociates.com>
2015-12-11 11:21:25 +0000

    Spec attributes using xpath

--- spec/helpers/assets_helper_spec.rb
+++ spec/helpers/assets_helper_spec.rb
@@ -27,8 +27,9 @@ describe AssetsHelper, type: :helper do
       expect(asset).to receive(:dimensions_for).with(:extended).and_return('80x60')
       expect(asset).to receive(:url_for).with(:extended).and_return(image_url)
 
-      expected_html = %Q{<img alt="Ladybird" height="60" src="#{image_url}" width="80" />}
-      expect(image_if_present(asset, :extended)).to eq expected_html
+      image = image_if_present(asset, :extended)
+      expect(image).to have_xpath('//img[@height="60"]')
+      expect(image).to have_xpath('//img[@width="80"]')
     end
 
     context "with :admin size" do
@@ -38,23 +39,25 @@ describe AssetsHelper, type: :helper do
       end
 
       it "has a valid image tag" do
-        expected_html = %Q{<img alt="Ladybird" height="180" src="#{image_url}" width="180" />}
-        expect(image_if_present(asset, :admin)).to eq expected_html
+        image = image_if_present(asset, :admin)
+        expect(image).to have_xpath('//img[@src="/media/ladybird.jpg"]')
+        expect(image).to have_xpath('//img[@height="180"]')
+        expect(image).to have_xpath('//img[@width="180"]')
       end
 
       it "has a valid image tag for string sizes" do
-        expected_html = %Q{<img alt="Ladybird" height="180" src="#{image_url}" width="180" />}
-        expect(image_if_present(asset, 'admin').to_s).to eq expected_html
+        image = image_if_present(asset, 'admin')
+        expect(image).to have_xpath('//img[@src="/media/ladybird.jpg"]')
+        expect(image).to have_xpath('//img[@height="180"]')
+        expect(image).to have_xpath('//img[@width="180"]')
       end
 
       it "includes additional properties passed" do
-        expected_html = %Q{<img alt="Ladybird" data-foo="bar" height="180" src="#{image_url}" width="180" />}
-        expect(image_if_present(asset, :admin, 'data-foo' => 'bar')).to eq expected_html
+        expect(image_if_present(asset, :admin, 'data-foo' => 'bar')).to have_xpath('//img[@data-foo="bar"]')
       end
 
       it "allows custom alt attributes" do
-        expected_html = %Q{<img alt="Ladybird" data-foo="bar" height="180" src="#{image_url}" width="180" />}
-        expect(image_if_present(asset, :admin, 'data-foo' => 'bar')).to eq expected_html
+        expect(image_if_present(asset, :admin, alt: 'bar')).to have_xpath('//img[@alt="bar"]')
       end
 
     end
@@ -70,8 +73,9 @@ describe AssetsHelper, type: :helper do
         expect(asset).to receive(:dimensions_for).with(:extended).and_return('80x60')
         expect(asset).to receive(:url_for).with(:extended).and_return(image_url)
 
-        expected_html = %Q{<img alt="Ladybird" height="60" src="#{image_url}" width="80" />}
-        expect(image_if_present(attachement, :extended)).to eq expected_html
+        image = image_if_present(attachement, :extended)
+        expect(image).to have_xpath('//img[@height="60"]')
+        expect(image).to have_xpath('//img[@width="80"]')
       end
 
     end
@@ -95,23 +99,25 @@ describe AssetsHelper, type: :helper do
       end
 
       it "is an image if there is no link" do
-        expected_html = %Q{<img alt="Ladybird" height="180" src="#{image_url}" width="180" />}
-        expect(link_image_if_linkable(nil, asset, :admin)).to eq expected_html
+        image = link_image_if_linkable(nil, asset, :admin)
+        expect(image).to have_xpath("//img[@src='#{image_url}']")
       end
 
       it "is a linked image if both are passsed" do
-        expected_html = %Q{<a href="/foo/bar"><img alt="Ladybird" height="180" src="#{image_url}" width="180" /></a>}
-        expect(link_image_if_linkable("/foo/bar", asset, :admin)).to eq expected_html
+        image = link_image_if_linkable('/foo/bar', asset, :admin)
+        expect(image).to have_xpath("//img[@src='#{image_url}']")
+        expect(image).to have_xpath("//a[@href='/foo/bar']")
       end
 
       it "allow options on the link" do
-        expected_html = %Q{<a class="baz" href="/foo/bar"><img alt="Ladybird" height="180" src="#{image_url}" width="180" /></a>}
-        expect(link_image_if_linkable("/foo/bar", asset, :admin, class: "baz")).to eq expected_html
+        image = link_image_if_linkable("/foo/bar", asset, :admin, class: "baz")
+        expect(image).to have_xpath("//a[@class='baz']")
       end
 
       it "allows options on the image as well" do
-        expected_html = %Q{<a class="arc" href="/foo/bar"><img alt="Ladybird" class="otter" height="180" src="#{image_url}" width="180" /></a>}
-        expect(link_image_if_linkable("/foo/bar", asset, :admin, class: "arc", image_options: {class: "otter"})).to eq expected_html
+        image = link_image_if_linkable("/foo/bar", asset, :admin, class: "arc", image_options: {class: "otter"})
+        expect(image).to have_xpath("//a[@class='arc']")
+        expect(image).to have_xpath("//img[@class='otter']")
       end
     end
   end
