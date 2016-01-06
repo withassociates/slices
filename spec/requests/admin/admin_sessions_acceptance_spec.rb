9e570c161021ca39c8f4fb67e23a25ad86631a1d
calumgunn <calum@withassociates.com>
2016-01-06 14:03:05 +0000

    Show error messages on Admin login

--- spec/requests/admin/admin_sessions_acceptance_spec.rb
+++ spec/requests/admin/admin_sessions_acceptance_spec.rb
@@ -16,6 +16,21 @@ describe "Authentication for /admin", type: :request, js: true do
     expect(page.current_path).to eq('/admin/site_maps')
   end
 
+  it "should show an error if password is incorrect" do
+    Admin.create!(email: 'hello@withassociates.com', password: '123456')
+    StandardTree.build_minimal
+
+    visit '/admin/sign_in'
+    expect(page).to have_no_css('header li a', text: 'Log out')
+
+    fill_in 'Email', with: 'hello@withassociates.com'
+    fill_in 'Password', with: 'incorrect'
+    click_on 'Sign in'
+
+    expect(page.current_path).to eq('/admin/sign_in')
+    expect(page).to have_content('Invalid')
+  end
+
   it "should redirect to sign in page if not signed in" do
     visit '/admin/site_maps'
     expect(page.current_path).to eq('/admin/sign_in')
