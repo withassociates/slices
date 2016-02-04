ece8d62aa549c9b43d82850349040bdece5a60f5
Thomas Marshall <thomas@thomasmarshall.com>
2016-02-04 16:31:22 +0000

    fixup! Redirect page path to external_url if set

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,10 +3,7 @@ require 'spec_helper'
 describe PagesController, type: :controller do
   context "GET :show" do
     let :page do
-      Page.new(
-        external_url: 'http://www.example.com',
-        active: true
-      )
+      Page.new(active: true, external_url: 'http://www.example.com')
     end
 
     it "redirects to the external url" do
